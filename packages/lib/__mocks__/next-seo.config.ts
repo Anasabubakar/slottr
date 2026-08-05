3e5121b794c9a80cd2b4592badfcc42c5a2d088e
Anas Abubakar <anasabubakar7000@gmail.com>
2026-08-05 17:32:23 +0100

    Add packages/lib (part 1/12)

---
+++ packages/lib/__mocks__/next-seo.config.ts
@@ -0,0 +1,17 @@
+vi.mock("@calcom/lib/next-seo.config", () => ({
+  default: {
+    headSeo: {
+      siteName: "Slottr",
+    },
+    defaultNextSeo: {
+      title: "Slottr",
+      description: "Scheduling infrastructure for everyone.",
+    },
+  },
+  seoConfig: {
+    headSeo: {
+      siteName: "Slottr",
+    },
+  },
+  buildSeoMeta: vi.fn().mockReturnValue({}),
+}));
