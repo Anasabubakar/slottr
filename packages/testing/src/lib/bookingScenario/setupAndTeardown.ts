49ecd8ee5e2c56894dcd6031d092ac3928c30176
Anas Abubakar <anasabubakar7000@gmail.com>
2026-08-06 16:22:33 +0100

    blind push

--- packages/testing/src/lib/bookingScenario/setupAndTeardown.ts
+++ packages/testing/src/lib/bookingScenario/setupAndTeardown.ts
@@ -1,17 +1,16 @@
 import { enableEmailFeature, mockNoTranslations } from "./bookingScenario";
-import process from "node:process";
-import { afterEach, beforeEach } from "vitest";
+import { afterEach, beforeEach, vi } from "vitest";
 
 export function setupAndTeardown() {
   beforeEach(() => {
     // Required to able to generate token in email in some cases
-    process.env.CALENDSO_ENCRYPTION_KEY = "abcdefghjnmkljhjklmnhjklkmnbhjui";
-    process.env.STRIPE_WEBHOOK_SECRET = "MOCK_STRIPE_WEBHOOK_SECRET";
+    vi.stubEnv("CALENDSO_ENCRYPTION_KEY", "abcdefghjnmkljhjklmnhjklkmnbhjui");
+    vi.stubEnv("STRIPE_WEBHOOK_SECRET", "MOCK_STRIPE_WEBHOOK_SECRET");
     // We are setting it in vitest.config.ts because otherwise it's too late to set it.
     // process.env.DAILY_API_KEY = "MOCK_DAILY_API_KEY";
 
     // Ensure that Rate Limiting isn't enforced for tests
-    delete process.env.UNKEY_ROOT_KEY;
+    vi.stubEnv("UNKEY_ROOT_KEY", undefined);
     mockNoTranslations();
     // mockEnableEmailFeature();
     enableEmailFeature();
@@ -19,9 +18,7 @@ export function setupAndTeardown() {
     fetchMock.resetMocks();
   });
   afterEach(() => {
-    delete process.env.CALENDSO_ENCRYPTION_KEY;
-    delete process.env.STRIPE_WEBHOOK_SECRET;
-    delete process.env.DAILY_API_KEY;
+    vi.unstubAllEnvs();
     globalThis.testEmails = [];
     fetchMock.resetMocks();
     // process.env.DAILY_API_KEY = "MOCK_DAILY_API_KEY";
